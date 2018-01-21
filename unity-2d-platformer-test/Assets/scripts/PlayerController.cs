a8ad653c69ffa08e73ca4fc440e0daca39437b5f
nicissimo97 <nici.regista@gmail.com>
2018-01-21 17:23:00 +0100

    PlayerController
    Moving and Jumping events added

---
+++ unity-2d-platformer-test/Assets/scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float movingSpeed;
+	public Rigidbody2D rigidBody;
+	public float jumpingSpeed;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatsGround;
+	public bool isGrounded;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatsGround);
+
+		if (Input.GetAxisRaw ("Horizontal") > 0f) {
+			rigidBody.velocity = new Vector3 (movingSpeed, rigidBody.velocity.y, 0f);
+		}
+		if (Input.GetAxisRaw ("Horizontal") < 0f) {
+			rigidBody.velocity = new Vector3 (-movingSpeed, rigidBody.velocity.y, 0f);
+		}
+
+		if (Input.GetButtonDown ("Jump") && isGrounded) {
+			rigidBody.velocity = new Vector3 (rigidBody.velocity.x, jumpingSpeed, 0f);
+		}
+	}
+}
